18e6b838cacb3f40befe22db917dfeecf4da52d0
Shi <zshi@fredhutch.org>
2019-07-16 12:12:13 -0700

    update regex code for type 1 report

--- 0_radiation.R
+++ 0_radiation.R
@@ -3,25 +3,46 @@
 
 library(stringr)
 
-
-t1 <- read.csv('C:/Users/Zaixing/OneDrive - Fred Hutchinson Cancer Research Center/0. Pathways/CVD project/Data/Rad_tx_notes_type1.csv',
+# load radiationd sample report
+t1 <- read.csv('Q:/HGREENLEE/Data Working/Pathways Heart Study/Radiation data/Rad_tx_notes_type1.csv',
+               stringsAsFactors=F)
+t2 <- read.csv('Q:/HGREENLEE/Data Working/Pathways Heart Study/Radiation data/Rad_tx_notes_type2.csv',
                stringsAsFactors=F)
 
-# remove line breaks
+
+# Preprocess type 1 report
+## remove line breaks
 t1[,1] <- str_replace_all(t1[,1], "[\r\n]" , "")
 
-# separate line by "?"
+## separate line by "?"
 tlist <- strsplit(t1[,1],"\\?")
 
-# single out radiation dose to date 
+## single out radiation dose to date 
 rad_dose <- sapply(tlist, function(x){
-  grep('Radiation dose|^Dose:|^DOSE:',unlist(x),value = T)
-})
-
-# single out radiation dose to date 
-rad_dose <- sapply(tlist, function(x){
-  length(grep('Gy',unlist(x),value = T))
+  grep('^.*?Dose|DOSE|dose.*?Gy.*?$',unlist(x),value = T)
 })
 
 
-# extract
+# extract data from type 1 report
+
+## cumulative dose
+sapply(rad_dose, function(x){
+  x <- unlist(x)
+  cum_dose <- str_extract(string = x, pattern = "date[:]\\s+[0-9]+\\s+cGy|[0-9]+\\s+cGy\\s+out")
+  cum_dose <- str_extract(string=cum_dose, pattern="[0-9]+")
+  #total_dose[!is.na(total_dose)]
+})
+       
+## total planned dose
+unlist(sapply(rad_dose, function(x){
+  x <- unlist(x)
+  total_dose <- str_extract(string = x, pattern = "of\\s*[A-Za-z]*\\s*[0-9]*\\s*cGy|of\\s*[0-9]*\\s*cGy")
+  total_dose <- str_extract(string=total_dose, pattern="[0-9]+")
+  total_dose <- total_dose[!is.na(total_dose)]
+  if(length(total_dose)==0){
+    NA
+  }
+  else {
+    total_dose
+  }
+}))
